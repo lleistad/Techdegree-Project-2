e474bb5b5de2bfce48ed9dbcabadb08643d7bc2c
Laura Leistad <laura.k.leistad@gmail.com>
2019-02-14 12:08:09 -0600

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,27 @@
-const studentList = document.querySelector("student-list");
+const studentList = document.getElementsByClassName("student-item cf");
 
-const showPage = (list, page = 1) => {
-  for (i = 0; i < list.length; i++) {
+const showPage = (studentList, page) => {
+  for (i = 0; i < studentList.length; i++) {
     const maxIndex = (page * 10) - 1;
-    const index = (page x 10) - 10;
+    const index = (page * 10) - 10;
     if (i >= index && i <= maxIndex) {
-      list[i].style.display = "block";
+      studentList[i].style.display = "block";
     } else {
-        list[i].style.display = "none";
+        studentList[i].style.display = "none";
     }
   }
 }
-
-showPage (studentList);
 
 
 //function that creates the pagination links and adds them to the page
 const appendPageLinks = (list) => {
     const totalNumberOfPages = Math.ceil(studentList.length/10);
-    const currentPage = 1;
+       
     //Create a div, give it the “pagination” class, and append it to the div with class of page
-    const paginationDiv = document.createElement("div");
+    const paginationDiv = document.createElement("div");  
     paginationDiv.setAttribute("class", "pagination");
     const pageDiv = document.querySelector("div.page");
-    document.body.insertBefore(paginationDiv, pageDiv);
+    pageDiv.appendChild(paginationDiv);
   
   
     //Add a ul to the “pagination” div to store the pagination links
@@ -31,21 +29,18 @@ const appendPageLinks = (list) => {
     paginationDiv.appendChild(ul);
   
     //Add li and a tags for each page. Then add event listener to each a tag so it'll call the showPage function, & apply active class to only the the link clicked.
-    for (i = 0; i < totalNumberOfPages.length; i++) {
+    for (i = 0; i < totalNumberOfPages; i=+1) {
+      const currentPage = 1;
       const li = document.createElement("li");
-      const aTag = () => {
-        const a = document.createElement("a");
-        const pageNumber = document.createTextNode(currentPage);
-        a.appendChild(pageNumber);     
-        a.addEventListener('click', showPage);  
-        a.classList.toggle("active", false);
-        event.target.classList.toggle("active", true);
-        currentPage++
-        }
-      li.appendChild(aTag);
+      const a = document.createElement("a");
+      a.innerHTML = currentPage;
+      a.addEventListener('click', showPage(studentList,i));  
+      a.classList.toggle("active", false);
+      Event.target.classList.toggle("active", true);
+      li.appendChild(a);
       ul.appendChild(li);
+      currentPage=+1;
      }
-  
-}
+  }
 
-appendPageLinks (studentList);
+appendPageLinks(studentList);
