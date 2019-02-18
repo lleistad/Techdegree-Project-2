ef1b780d0267e7d7e19f2df398cd39770f6ffec7
Laura Leistad <laura.k.leistad@gmail.com>
2019-02-18 11:19:47 -0600

    Update js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 
 const studentList = document.getElementsByClassName("student-item cf");
 
-const showPage = (studentList, page) => {
+const showPage = (list, page) => {
   for (i = 0; i < studentList.length; i++) {
     const maxIndex = (page * 10) - 1;
     const index = (page * 10) - 10;
@@ -35,17 +35,20 @@ const appendPageLinks = (list) => {
   
     //Add li and a tags for each page. Then add event listener to each a tag so it'll call the showPage function, & apply active class to only the the link clicked.
     for (i = 0; i < totalNumberOfPages; i=+1) {
-      const currentPage = 1;
+      let currentPage = 1;
       const li = document.createElement("li");
       const a = document.createElement("a");
       a.innerHTML = currentPage;
-      a.addEventListener('click', showPage(studentList,i));  
-      a.classList.toggle("active", false);
-      Event.target.classList.toggle("active", true);
       li.appendChild(a);
       ul.appendChild(li);
-      currentPage=+1;
-     }
+      currentPage++;
+        }
+    a.addEventListener('click', (e) => {
+          showPage(studentList, i);  
+          a.classList.toggle("active", false);
+          Event.target.classList.toggle("active", true);                   
+          });
   }
-
+  
+showPage();
 appendPageLinks(studentList);
